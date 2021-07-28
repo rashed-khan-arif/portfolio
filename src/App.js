ff171eab6b2c81f8ab7a02fa6a7804a053b139c5
Rashed Khan Arif <arifkhan0713@gmail.com>
2021-07-28 20:04:46 +0600

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Rashed Khan Arif
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://linkedin.com/in/rashedkhanarif"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Welcome
         </a>
       </header>
     </div>
